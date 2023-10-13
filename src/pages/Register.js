ae500467692b6fa67aaf1e6849749c626d9e0661
p4avinash <p4avinashkumar@gmail.com>
2023-10-13 10:10:44 +0530

    implemented the register and login routes

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react"
 import { Logo, FormRow } from "../components"
 import Wrapper from "../assets/wrappers/RegisterPage"
 import { toast } from "react-toastify"
+import { useDispatch, useSelector } from "react-redux"
+import { loginUser, registerUser } from "../features/user/userSlice"
 
 const initialState = {
   name: "",
@@ -12,6 +14,8 @@ const initialState = {
 
 const Register = () => {
   const [values, setValues] = useState(initialState)
+  const { user, isLoading } = useSelector((store) => store.user)
+  const dispatch = useDispatch()
 
   const handleChange = (e) => {
     const name = e.target.name
@@ -25,7 +29,14 @@ const Register = () => {
 
     if (!email || !password || (!isMember && !name)) {
       toast.error("Please fill out all fields")
+      return
     }
+
+    if (isMember) {
+      dispatch(loginUser({ email: email, password: password }))
+      return
+    }
+    dispatch(registerUser({ email, password, name }))
   }
 
   const toggleMember = () => {
@@ -64,8 +75,8 @@ const Register = () => {
           labelText='password'
           handleChange={handleChange}
         />
-        <button type='submit' className='btn btn-block'>
-          submit
+        <button type='submit' className='btn btn-block' disabled={isLoading}>
+          {isLoading ? "loading..." : "submit"}
         </button>
         <p>
           {values.isMember ? "Not a member yet ?" : "Already a member ?"}
